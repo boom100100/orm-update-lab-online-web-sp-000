e0b2b35881b89d05171c042736008d5b77bbf49f
Bernadette Davis <bsd231@nyu.edu>
2019-08-30 15:34:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = \"?\";"
+    sql = "SELECT * FROM students WHERE name = ?;"
     row = DB[:conn].execute(sql, name)[0]
 
     student = Student.new(row[1], row[2], row[0])
